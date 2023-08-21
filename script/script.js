d92983fe69005157a8d8090990026795f6640528
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-08-21 17:23:16 +0600

    rsponsive and all done

--- script/script.js
+++ script/script.js
@@ -1,12 +1,40 @@
+const couponCodeInput = document.getElementById("couponCodeInput");
+const applyCouponButton = document.getElementById("applyCouponButton");
+const makePurchaseButton = document.getElementById("makePurchaseButton");
+const totalAfterDiscountElement = document.getElementById("totalAfterDiscount");
+
 let total = 0;
+let discount = 0;
+
+applyCouponButton.disabled = true;
+applyCouponButton.style.backgroundColor = "gray";
+makePurchaseButton.disabled = true;
+makePurchaseButton.style.backgroundColor = "gray";
+
+couponCodeInput.addEventListener("input", () => {
+    const couponValue = couponCodeInput.value.trim();
+    applyCouponButton.disabled = couponValue !== "SELL200";
+    applyCouponButton.style.backgroundColor = couponValue === "SELL200" ? "#E527B2" : "gray";
+});
+
+applyCouponButton.addEventListener("click", () => {
+    if (couponCodeInput.value.trim() === "SELL200") {
+        applyCouponButton.style.backgroundColor = "#E527B2";
+        if (total >= 200) {
+            discount = parseFloat(total) * 0.2;
+            totalAfterDiscountElement.innerText = (total - discount).toFixed(2) + "TK";
+            document.getElementById("discount").innerText = discount.toFixed(2) + "TK";
+        }
+    }
+});
 
 
 function handleClick(target) {
     const selectedItemsContainer = document.getElementById("selected-items");
-    const kitchenWareItems = target.childNodes[5].childNodes[1].innerText;
+    const allItems = target.childNodes[5].childNodes[1].innerText;
 
     const li = document.createElement('li');
-    li.innerText = kitchenWareItems;
+    li.innerText = allItems;
 
     selectedItemsContainer.appendChild(li);
 
@@ -15,12 +43,11 @@ function handleClick(target) {
     total = (parseFloat(total) + parseFloat(price)).toFixed(2);
     document.getElementById("totalPrice").innerText = total + "TK";
 
-    if (total >= 200) {
-        const discount = (parseFloat(total) * 0.2).toFixed(2);
-        document.getElementById("discount").innerText = discount + "TK";
+    if (total > 0) {
+        makePurchaseButton.disabled = false;
+        makePurchaseButton.style.backgroundColor = "#E527B2";
+    } else {
+        makePurchaseButton.disabled = true;
+        makePurchaseButton.style.backgroundColor = "gray";
     }
-    const newTotal = total - discount;
-    document.getElementById("totalAfterDiscount").innerText = parseFloat(newTotal) + "TK";
-
 }
-
